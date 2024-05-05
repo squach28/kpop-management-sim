2d69752e0dc6c5ccdbf743bbf8b1dd489dec3ee8
squach28 <squach3443@gmail.com>
2024-05-05 15:35:40 -0700

    added bodyParser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
