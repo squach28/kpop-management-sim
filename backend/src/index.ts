addedd63351725fb3e3ea7ff3a67823ceac2fb33
squach28 <squach3443@gmail.com>
2024-05-06 14:58:16 -0700

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { idolRoutes } from "./routes/idolRoutes";
+import cors from "cors";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 app.use("/idols", idolRoutes);
