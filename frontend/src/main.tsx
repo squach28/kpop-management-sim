de7e342d55de609b834b6173ddfe3daa1ebc5c76
squach28 <squach3443@gmail.com>
2024-05-06 11:32:32 -0700

    added react-router-dom and chakra

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>,
+);
