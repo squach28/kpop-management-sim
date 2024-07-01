d65ea608012ad76bda2c18e8ed965311970e0251
squach28 <squach3443@gmail.com>
2024-07-01 12:47:53 -0700

    added Idol type, show idols in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import { Idol } from "./types/Idol";
 
 const App = () => {
+  const [idols, setIdols] = useState<Idol[]>([]);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/idols`)
+      .then((res) => res.json())
+      .then((data: Idol[]) => {
+        setIdols(data);
+      });
+  }, []);
+
   return (
     <>
       <NavBar />
+      {idols.map((idol) => (
+        <div key={idol.id}>
+          <p>{idol.name}</p>
+        </div>
+      ))}
     </>
   );
 };
---
+++ frontend/src/types/Idol.ts
@@ -0,0 +1,8 @@
+export type Idol = {
+  id: number;
+  name: string;
+  image_url: string;
+  birth_name: string;
+  nationality: string;
+  dob: Date;
+};
